131b91f6f5fa8383022098dcd97618d9676fdf40
Gagan Chordia <chordiagagan@gmail.com>
2024-07-21 20:41:29 +0530

    added db setup

---
+++ backend/database/setup.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"dot_conf/configs"
+	"dot_conf/models"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Initialize() error {
+	var err error
+
+	config := configs.NewDatabaseConfig()
+	connectionString := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+		config.Host, config.Port, config.User, config.Password, config.DbName, config.SslMode,
+	)
+	db, err = gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+
+	if err != nil {
+		return err
+	}
+
+	log.Info("Initialized the db successfully")
+
+	err = db.AutoMigrate(
+		&models.Comment{},
+		&models.Service{},
+		&models.User{},
+		&models.Config{},
+		&models.Company{},
+		&models.ConfigRequest{},
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
--- backend/models/schemas.go
+++ backend/models/schemas.go
@@ -1,18 +1,18 @@
 package models
 
 type Company struct {
-	ID           int64  `json:"id"`
+	ID           int64  `json:"id" gorm:"primaryKey;autoIncrement:true"`
 	Name         string `json:"name"`
-	Email        string `json:"email"`
-	AdminId      string `json:"admin_id"`
+	Email        string `json:"email" gorm:"unique"`
+	AdminId      string `json:"admin_id" gorm:"unique"`
 	RegisteredOn string `json:"registered_on"`
 	ModifiedOn   string `json:"modified_on,omitempty"`
 	DocumentPath string `json:"document_path"`
 }
 
 type User struct {
-	ID           string     `json:"id"`
-	Email        string     `json:"email"`
+	ID           string     `json:"id" gorm:"primaryKey"`
+	Email        string     `json:"email" gorm:"unique"`
 	Name         string     `json:"name"`
 	Password     string     `json:"password"`
 	Role         Role       `json:"role"`
@@ -22,11 +22,11 @@ type User struct {
 }
 
 type Service struct {
-	ID         string   `json:"id"`
+	ID         string   `json:"id" gorm:"primaryKey"`
 	Name       string   `json:"name"`
 	Owners     []string `json:"owners"`
 	CompanyID  int64    `json:"company_id"`
-	ServiceKey string   `json:"service_key"`
+	ServiceKey string   `json:"service_key" gorm:"unique"`
 	CreatedBy  string   `json:"created_by"`
 	CreatedOn  string   `json:"created_on"`
 	ModifiedBy string   `json:"modified_by,omitempty"`
@@ -34,7 +34,7 @@ type Service struct {
 }
 
 type Config struct {
-	ID          string `json:"id"`
+	ID          string `json:"id" gorm:"primaryKey"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
 	Type        Type   `json:"type"`
@@ -47,7 +47,7 @@ type Config struct {
 }
 
 type ConfigRequest struct {
-	ID         int64         `json:"id"`
+	ID         int64         `json:"id" gorm:"primaryKey,autoIncrement:true"`
 	ServiceId  string        `json:"service_id"`
 	ConfigID   string        `json:"config_id"`
 	ApprovedBy string        `json:"approved_by,omitempty"`
@@ -56,7 +56,7 @@ type ConfigRequest struct {
 }
 
 type Comment struct {
-	ID        int64  `json:"id"`
+	ID        int64  `json:"id" gorm:"primaryKey,autoIncrement:true"`
 	RequestID string `json:"request_id"`
 	Message   string `json:"message"`
 	UserEmail string `json:"user_email"`
